7c8a9720d01c4120bc94fcbaf951578274faff9c
Jindrich Skupa <jindrich.skupa@gmail.com>
2015-11-10 19:52:44 +0100

    aded bank_account into config

--- lib/payday/config.rb
+++ lib/payday/config.rb
@@ -2,7 +2,7 @@ module Payday
   # Configuration for Payday. This is a singleton, so to set the company_name you would call
   # Payday::Config.default.company_name = "Awesome Corp".
   class Config
-    attr_accessor :invoice_logo, :company_name, :company_details, :date_format, :currency, :pdf_font_families, :pdf_font, :pdf_font_size
+    attr_accessor :invoice_logo, :company_name, :company_details, :date_format, :currency, :pdf_font_families, :pdf_font, :pdf_font_size, :bank_account
 
     # Sets the page size to use. See the
     # {http://prawn.majesticseacreature.com/docs/0.10.2/Prawn/Document/PageGeometry.html Prawn documentation} for valid
@@ -22,6 +22,7 @@ module Payday
       self.invoice_logo = File.join(File.dirname(__FILE__), "..", "..", "spec", "assets", "default_logo.png")
       self.company_name = "Awesome Corp"
       self.company_details = "awesomecorp@commondream.net"
+      self.bank_account = "1234567890/1234"
       self.date_format = "%B %e, %Y"
       self.currency = "USD"
       self.page_size = "LETTER"
